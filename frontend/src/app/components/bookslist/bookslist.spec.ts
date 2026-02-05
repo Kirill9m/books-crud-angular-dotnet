120227d27928550136d48b007e34d92961fc1da4
kirill <kirillsavorin@gmail.com>
2026-02-05 16:40:40 +0100

    Books list mock

---
+++ frontend/src/app/components/bookslist/bookslist.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Bookslist } from './bookslist';
+
+describe('Bookslist', () => {
+  let component: Bookslist;
+  let fixture: ComponentFixture<Bookslist>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Bookslist]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Bookslist);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
