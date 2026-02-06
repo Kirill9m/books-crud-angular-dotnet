e20808e19bc806050726633cc122b41101d872fd
kirill <kirillsavorin@gmail.com>
2026-02-06 10:51:17 +0100

    feat: navbar, theme, router

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,14 +1,30 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, DOCUMENT, inject, signal } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { Bookslist } from './components/bookslist/bookslist';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faLightbulb } from '@fortawesome/free-solid-svg-icons';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [Bookslist, FontAwesomeModule],
+  imports: [FontAwesomeModule, RouterLink, RouterOutlet, FontAwesomeModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('frontend');
+  private readonly document = inject(DOCUMENT);
+  faBulb = faLightbulb;
+
+  toggleTheme() {
+    const html = this.document.documentElement;
+    const current = html.getAttribute('data-bs-theme') ?? 'light';
+    html.setAttribute('data-bs-theme', current === 'dark' ? 'light' : 'dark');
+    if (current === 'dark') {
+      this.themeBtn = 'text-secondary';
+    } else {
+      this.themeBtn = 'text-warning';
+    }
+  }
+
+  themeBtn = 'text-warning';
 }
