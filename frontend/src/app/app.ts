3ef4a4ff59da8b5a2d4161caf84c4ee3bb409bd4
kirill <kirillsavorin@gmail.com>
2026-02-06 13:41:36 +0100

    ngStyle example

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,14 +1,13 @@
-import { Component, DOCUMENT, inject, signal } from '@angular/core';
+import { Component, DOCUMENT, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { Bookslist } from './components/bookslist/bookslist';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faLightbulb } from '@fortawesome/free-solid-svg-icons';
 import { FormsModule } from '@angular/forms';
-import { NgClass } from '@angular/common';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [FontAwesomeModule, RouterLink, RouterOutlet, FontAwesomeModule, FormsModule, NgClass],
+  imports: [FontAwesomeModule, RouterLink, RouterOutlet, FontAwesomeModule, FormsModule, NgStyle],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
@@ -20,11 +19,9 @@ export class App {
     const html = this.document.documentElement;
     const current = html.getAttribute('data-bs-theme') ?? 'light';
     html.setAttribute('data-bs-theme', current === 'dark' ? 'light' : 'dark');
+    this.isThemeDark = !this.isThemeDark;
   }
 
-  setThemeBtnColor(className: string) {
-    this.themeBtn.set(className);
-  }
-
-  themeBtn = signal<string>('text-secondary');
+  isThemeDark: boolean = true;
 }
+
