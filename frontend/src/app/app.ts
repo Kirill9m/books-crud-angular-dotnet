9adf38243c968220a0fa5be4894c8c47d4ffdb60
kirill <kirillsavorin@gmail.com>
2026-02-06 14:09:33 +0100

    Init's and pipelines

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,17 +1,25 @@
-import { Component, DOCUMENT, inject } from '@angular/core';
+import { Component, DOCUMENT, inject, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faLightbulb } from '@fortawesome/free-solid-svg-icons';
 import { FormsModule } from '@angular/forms';
-import { NgStyle } from '@angular/common';
+import { NgStyle, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [FontAwesomeModule, RouterLink, RouterOutlet, FontAwesomeModule, FormsModule, NgStyle],
+  imports: [
+    FontAwesomeModule,
+    RouterLink,
+    RouterOutlet,
+    FontAwesomeModule,
+    FormsModule,
+    NgStyle,
+    TitleCasePipe,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
   private readonly document = inject(DOCUMENT);
   faBulb = faLightbulb;
 
@@ -19,8 +27,9 @@ export class App {
     const html = this.document.documentElement;
     const current = html.getAttribute('data-bs-theme') ?? 'light';
     html.setAttribute('data-bs-theme', current === 'dark' ? 'light' : 'dark');
-    this.isThemeDark = !this.isThemeDark;
   }
-
+  ngOnInit(): void {
+    console.log('App initialized');
+  }
   isThemeDark: boolean = true;
 }
